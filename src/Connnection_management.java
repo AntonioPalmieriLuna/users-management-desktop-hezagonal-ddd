297ee8d612c38a997baf24f6f95101d48f32f718
AntonioPalmieriLuna <apalmieril@unicartagena.edu.co>
2026-05-20 22:13:36 -0500

    Se agregó el archivo connection management a la rama feature prueba

---
+++ src/Connnection_management.java
@@ -0,0 +1,5 @@
+public class Connnection_management {
+    public static void main(String[]args){
+        System.out.println("Este es el connection manager para el los usuarios - prueba creada para la rama ");
+    }
+}
